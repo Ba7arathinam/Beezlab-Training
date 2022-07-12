e36a5e0849e14d4de98e0bffbcf69e87ad6b8660
Ba7arathinam <109063403+Ba7arathinam@users.noreply.github.com>
2022-07-12 18:50:25 +0530

    Add files via upload

---
+++ Electricity Bill.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+        int custid, custu;
+        string cusnum;
+        double chg = 0, surchg=0 , chrgamt, netamt;
+
+        Console.WriteLine("Input Customer ID:");
+        custid = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input the name of the customer:");
+        cusnum = Console.ReadLine();
+        Console.WriteLine("Input the unit consumed by the customer:");
+        custu = Convert.ToInt32(Console.ReadLine());
+
+        if (custu < 200)
+        {
+            Console.WriteLine(chg = 1.20);
+        }
+        else if (custu >= 200 && custu < 400)
+        {
+            Console.WriteLine(chg = 1.50);
+        }
+        else if (custu >= 400 && custu < 600)
+        {
+            Console.WriteLine(chg = 1.80);
+        }
+        else if (custu > 600)
+        {
+            Console.WriteLine(chg = 2.00);
+        }
+
+        chrgamt = custu * chg;
+        if (chrgamt > 400)
+            surchg = chrgamt * 15 / 100.0;
+        netamt = chrgamt + surchg;
+        if (netamt < 100)
+            netamt = 100;
+        Console.WriteLine("\nElectricity Bill\n");
+        Console.WriteLine("Customer IDNO                       :" + custid);
+        Console.WriteLine("Customer Name                       :"+ cusnum);
+        Console.WriteLine("unit Consumed                       :"+ custu);
+        Console.WriteLine("Amount Charges   per unit :" +chg , chrgamt);
+        Console.WriteLine("Surchage Amount                     :"+ surchg);
+        Console.WriteLine("Net Amount Paid By the Customer     :"+ netamt);
+    
+
+
+
---
+++ Grade sum.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    string input;
+Console.WriteLine("E = Excellent");
+Console.WriteLine("V = Very Good");
+Console.WriteLine("G = Good");
+Console.WriteLine("A = Average");
+Console.WriteLine("F = Fail");
+Console.WriteLine("Input the grade:");
+input = Console.ReadLine();
+if(input == "E")
+{
+    Console.WriteLine("Excellent");
+}
+else if(input == "V"||input =="v")
+{
+    Console.WriteLine("Very Good");
+}
+else if(input == "G"|| input=="g")
+{
+    Console.WriteLine("Good");
+}
+else if(input == "A"||input=="a")
+{
+    Console.WriteLine("Average");
+}
+else if(input == "F"||input=="f")
+{
+    Console.WriteLine("Fail");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+Console.ReadLine();
